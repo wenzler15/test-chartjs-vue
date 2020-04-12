9a7fe42bb52cf8bfa2d06646a76503ca559ad859
Robert Lewis <rswl86@gmail.com>
2020-04-12 13:20:24 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,169 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col">
+        <h1 class="text-center">COVID-19 DATA</h1>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrPositive.length > 0">
+      <div class="col">
+        <h2 class="text-center">Positive</h2>
+        <line-chart
+          :chartData="arrPositive"
+          :options="chartOptions"
+          :chartColors="positiveChartColors"
+          label="Positive"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrHospitalized.length > 0">
+      <div class="col">
+        <h2 class="text-center">Hospitalized</h2>
+        <line-chart
+          :chartData="arrHospitalized"
+          :options="chartOptions"
+          :chartColors="hospitalizedChartColors"
+          label="Hospitalized"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrInIcu.length > 0">
+      <div class="col">
+        <h2 class="text-center">In ICU</h2>
+        <line-chart
+          :chartData="arrInIcu"
+          :options="chartOptions"
+          :chartColors="inIcuColors"
+          label="In ICU"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrOnVentilators.length > 0">
+      <div class="col">
+        <h2 class="text-center">On Ventilators</h2>
+        <line-chart
+          :chartData="arrOnVentilators"
+          :options="chartOptions"
+          :chartColors="onVentilatorsColors"
+          label="On Ventilators"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrRecovered.length > 0">
+      <div class="col">
+        <h2 class="text-center">Recovered</h2>
+        <line-chart
+          :chartData="arrRecovered"
+          :options="chartOptions"
+          :chartColors="recoveredColors"
+          label="Recovered"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5 mb-5">
+      <div class="col">
+        <h2 class="text-center">Deaths</h2>
+        <line-chart
+          v-if="arrDeaths.length > 0"
+          :chartData="arrDeaths"
+          :options="chartOptions"
+          :chartColors="deathColors"
+          label="Deaths"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import moment from "moment";
+
+import LineChart from "./components/LineChart";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    LineChart
+  },
+  data() {
+    return {
+      arrPositive: [],
+      positiveChartColors: {
+        borderColor: "#077187",
+        pointBorderColor: "#0E1428",
+        pointBackgroundColor: "#AFD6AC",
+        backgroundColor: "#74A57F"
+      },
+      arrHospitalized: [],
+      hospitalizedChartColors: {
+        borderColor: "#251F47",
+        pointBorderColor: "#260F26",
+        pointBackgroundColor: "#858EAB",
+        backgroundColor: "#858EAB"
+      },
+      arrInIcu: [],
+      inIcuColors: {
+        borderColor: "#190B28",
+        pointBorderColor: "#190B28",
+        pointBackgroundColor: "#E55381",
+        backgroundColor: "#E55381"
+      },
+      arrOnVentilators: [],
+      onVentilatorsColors: {
+        borderColor: "#784F41",
+        pointBorderColor: "#784F41",
+        pointBackgroundColor: "#BBE5ED",
+        backgroundColor: "#BBE5ED"
+      },
+      arrRecovered: [],
+      recoveredColors: {
+        borderColor: "#4E5E66",
+        pointBorderColor: "#4E5E66",
+        pointBackgroundColor: "#31E981",
+        backgroundColor: "#31E981"
+      },
+      arrDeaths: [],
+      deathColors: {
+        borderColor: "#E06D06",
+        pointBorderColor: "#E06D06",
+        pointBackgroundColor: "#402A2C",
+        backgroundColor: "#402A2C"
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  async created() {
+    const { data } = await axios.get("https://covidtracking.com/api/us/daily");
+
+    data.forEach(d => {
+      const date = moment(d.date, "YYYYMMDD").format("MM/DD");
+      const {
+        positive,
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death
+      } = d;
+
+      this.arrPositive.push({ date, total: positive });
+      this.arrHospitalized.push({ date, total: hospitalizedCurrently });
+      this.arrInIcu.push({ date, total: inIcuCurrently });
+      this.arrOnVentilators.push({ date, total: onVentilatorCurrently });
+      this.arrRecovered.push({ date, total: recovered });
+      this.arrDeaths.push({ date, total: death });
+    });
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,49 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String
+    },
+    chartData: {
+      type: Array
+    },
+    options: {
+      type: Object
+    },
+    chartColors: {
+      type: Object
+    }
+  },
+  mounted() {
+    const dates = this.chartData.map(d => d.date).reverse();
+    const totals = this.chartData.map(d => d.total).reverse();
+
+    const {
+      borderColor,
+      pointBorderColor,
+      pointBackgroundColor,
+      backgroundColor
+    } = this.chartColors;
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals,
+            borderColor: borderColor,
+            pointBorderColor: pointBorderColor,
+            pointBackgroundColor: pointBackgroundColor,
+            backgroundColor: backgroundColor
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
